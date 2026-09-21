3bd830fe5b8fc4e531e731f7b5abbb858335e675
vedharaj <crisvedharaj@gmail.com>
2026-09-21 19:18:43 +0530

    feat: add search and delete operations to LinkedListDS

--- Data Structure/LinkedListDS.java
+++ Data Structure/LinkedListDS.java
@@ -32,6 +32,34 @@ class LinkedList<T>{
         }
         System.out.println();
     }
+    
+    public int search(T data){
+        int idx = 0;
+        Note<T> cur = head;
+        while(cur != null){
+            if(cur.data == data) return idx;
+            idx++;
+            cur=cur.next;
+        }
+        return -1;
+    }
+
+    public void delete(T data){
+        Note<T> prev = null;
+        Note<T> cur = head;
+        while(cur != null){
+          if(cur.data == data){
+              System.out.println("Successfully deleted "+cur.data);
+              prev.next = cur.next;
+              System.out.print("Current values are ");
+              this.display();
+              return;
+          }
+          prev = cur;
+          cur=cur.next;
+        }
+        System.out.println("Given value cannot find!");
+    }
 }
 
 public class LinkedListDS
@@ -41,7 +69,7 @@ public class LinkedListDS
 		Scanner sc = new Scanner(System.in);
 		int c, v;
 		while(true){
-		    System.out.println("LinkedList DataType\n1. Add\n2. Display\n3. exit\n");
+		    System.out.println("LinkedList DataType\n1. Add\n2. Display\n3. Search\n4. Delete\n5. exit\n");
 		    c = sc.nextInt();
 		    switch(c){
 		        case 1:{
@@ -51,7 +79,24 @@ public class LinkedListDS
 		            break;
 		        }
 		        case 2: ll.display();break;
-		        case 3: sc.close();System.exit(0);
+		        case 3: {
+		            System.out.print("Enter Value: ");
+		            v = sc.nextInt();
+		            int res = ll.search(v);
+		            if(res==-1){
+		             System.out.println("Not Found");
+		            } else{
+		              System.out.println(v+" is at "+res);
+		            }
+		            break;
+		        }
+		        case 4:{
+		          System.out.print("Enter Value: ");
+		          v = sc.nextInt();
+		          ll.delete(v);
+		          break;
+		        }
+		        case 5: sc.close();System.exit(0);
 		    }
 		}
 	}
